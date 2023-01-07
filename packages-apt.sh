89fbd79a047a0b4492c6df67b802af26ba4cdfb3
Andy Cheng <atc2150@columbia.edu>
2023-01-07 17:48:34 -0500

    Added vim-gtk prompt

--- packages-apt.sh
+++ packages-apt.sh
@@ -2,9 +2,21 @@
 # Must be run as superuser
 
 # Required for kernel compilation
-apt install -y sudo git build-essential net-tools linux-headers-`uname -r` \
+apt install -y sudo git build-essential net-tools \
 	openssh-server bc python bison flex libelf-dev libssl-dev \
 	libncurses-dev dwarves
 
+# Needed for kernel module development
+apt install -y linux-headers-`uname -r`
+
 # Required for vim
 apt install -y vim-gtk clang cmake curl python3 python3-dev
+
+echo
+echo
+echo
+echo "Choose vim-gtk"
+echo
+echo
+echo
+update-alternatives --config vim
