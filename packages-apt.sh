5496b62f960e287f3e3ee4127d4c5fee899e2ec4
Andy Cheng <atc2150@columbia.edu>
2023-01-24 22:02:09 -0500

    Added tmux as an installed package

--- packages-apt.sh
+++ packages-apt.sh
@@ -7,20 +7,22 @@ apt install -y sudo git openssh-server
 apt install -y build-essential net-tools bison flex libelf-dev \
 	libssl-dev libncurses-dev dwarves bc
 
-# Might not need python
+# Might not need python2
 # apt install -y python
 
 # Needed for kernel module development
 apt install -y linux-headers-`uname -r`
 
+## Editing Environment
 # Required for vim
 apt install -y vim-gtk clang cmake cscope curl python3 python3-dev
 
-echo
+# tmux and other management
+apt install -y tmux
+
 echo
 echo
 echo "Choose vim-gtk"
-echo
 echo
 echo
 update-alternatives --config vim
