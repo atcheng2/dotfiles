11bef9cf6107e1f51eef566d4d639e178192d400
Andy Cheng <atc2150@columbia.edu>
2023-02-05 17:59:17 -0500

    Added quotations to references to dir in case of directories with spaces

--- install.sh
+++ install.sh
@@ -5,16 +5,16 @@ dir=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
 
 echo "Linking dotfiles..."
 
-ln -sf $dir/vimrc ~/.vimrc
-ln -sf $dir/light.vim ~/.light.vim
-ln -sf $dir/bash_aliases ~/.bash_aliases
-ln -sf $dir/gitconfig ~/.gitconfig
-ln -sf $dir/gitignore ~/.gitignore
-ln -sf $dir/tmux.conf ~/.tmux.conf
+ln -sf "$dir/vimrc" ~/.vimrc
+ln -sf "$dir/light.vim" ~/.light.vim
+ln -sf "$dir/bash_aliases" ~/.bash_aliases
+ln -sf "$dir/gitconfig" ~/.gitconfig
+ln -sf "$dir/gitignore" ~/.gitignore
+ln -sf "$dir/tmux.conf" ~/.tmux.conf
 
 # .config subdirectories
 mkdir -p ~/.config
 sudo rm -rf ~/.config/clangd
-ln -sf $dir/config/clangd ~/.config/clangd
+ln -sf "$dir/config/clangd" ~/.config/clangd
 
 echo "Done"
